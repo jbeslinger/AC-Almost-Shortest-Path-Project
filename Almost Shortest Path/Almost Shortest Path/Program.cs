7542ba8e7edbb48a66c762b3031cb6e68e7eb07e
jbeslinger <jbeslinger@gmail.com>
2022-02-14 15:50:41 -0500

    init visual studio solution

---
+++ Almost Shortest Path/Almost Shortest Path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Almost_Shortest_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
